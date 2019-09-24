a003842540d084bceb5218a184bec57fca10b181
Morgane Mboma <54062684+mbomamorgane@users.noreply.github.com>
2019-09-24 15:19:47 +0200

    ok

---
+++ cookie.js
@@ -0,0 +1,5 @@
+score=0;
+function cliker(){
+    score+=1;
+    document.getElementById("barrescore").innerHTML=score;
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+function test(){
+    alert(jfksjdfk);
+}
+
+ImageCookie.addEventListener('click', test);
