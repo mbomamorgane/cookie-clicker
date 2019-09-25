a4ea88291524df0dfe7d6220c6f1d31c271d3bef
Morgane Mboma <54062684+mbomamorgane@users.noreply.github.com>
2019-09-25 09:34:18 +0200

    ok

--- cookie.js
+++ cookie.js
@@ -1,5 +1,19 @@
-score=0;
+let multibutton = document.getElementById("multiplier");
+let score = 0;
+let price = 50;
+let multiplicateur = 1;
+let multiplierCost = 20;
+
+
 function cliker(){
     score+=1;
     document.getElementById("barrescore").innerHTML=score;
 }
+
+function displayMultiplier(multiplicateur) {
+    for (var i= 1; i <= multiplicateur; i++) {
+      score++;
+    }
+    document.getElementById("barrescore").innerHTML=score+displayMultiplier;
+  }
+
